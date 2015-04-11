7ef656926737026e7a5e051b65f57320ba680bc4
Martin Angers <martin.n.angers@gmail.com>
2015-04-11 10:49:44 -0400

    test: CharClassMatcher

--- targeted_test.go
+++ targeted_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+	"unicode"
 )
 
 func TestParseNoRule(t *testing.T) {
@@ -93,6 +94,10 @@ func TestParseLitMatcher(t *testing.T) {
 		{"ab", "abc", true, nil, false},
 		{"\u2190a", "\u2190", false, []byte("\u2190"), true},
 		{"\u2190a", "\u2190", true, []byte("\u2190"), true},
+		{"\n", "\n", false, []byte("\n"), true},
+		{"\n", "\n", true, []byte("\n"), true},
+		{"\na", "\n", false, []byte("\n"), true},
+		{"\na", "\n", true, []byte("\n"), true},
 	}
 
 	for _, tc := range cases {
@@ -113,6 +118,94 @@ func TestParseLitMatcher(t *testing.T) {
 		}
 		if ok != tc.match {
 			t.Errorf("%s: want match? %t, got %t", lbl, tc.match, ok)
+		}
+		if p.pt.offset != len(tc.out) {
+			t.Errorf("%s: want offset %d, got %d", lbl, len(tc.out), p.pt.offset)
+		}
+	}
+}
+
+func TestParseCharClassMatcher(t *testing.T) {
+	cases := []struct {
+		in      string
+		val     string
+		chars   []rune
+		ranges  []rune
+		classes []string
+		ic      bool
+		iv      bool
+		out     []byte
+	}{
+		{in: "", val: "[]", out: nil}, // empty char class means no char matches
+		{in: "", val: "[^]", iv: true, out: []byte{}},
+		{in: "", val: "[]i", ic: true, out: nil},
+		{in: "", val: "[^]i", ic: true, iv: true, out: []byte{}},
+		{in: "a", val: "[]", out: nil},
+		{in: "a", val: "[^]", iv: true, out: []byte("a")},
+		{in: "a", val: "[]i", ic: true, out: nil},
+		{in: "a", val: "[^]i", ic: true, iv: true, out: []byte("a")},
+
+		{in: "a", val: "[a]", chars: []rune{'a'}, out: []byte("a")},
+		{in: "a", val: "[a]i", ic: true, chars: []rune{'a'}, out: []byte("a")},
+		{in: "A", val: "[a]i", ic: true, chars: []rune{'a'}, out: []byte("A")},
+		{in: "a", val: "[^a]", chars: []rune{'a'}, iv: true, out: nil},
+		{in: "A", val: "[^a]i", iv: true, ic: true, chars: []rune{'a'}, out: nil},
+
+		{in: "b", val: "[a]", chars: []rune{'a'}, out: nil},
+		{in: "b", val: "[a]i", ic: true, chars: []rune{'a'}, out: nil},
+		{in: "B", val: "[a]i", ic: true, chars: []rune{'a'}, out: nil},
+		{in: "b", val: "[^a]", chars: []rune{'a'}, iv: true, out: []byte("b")},
+		{in: "b", val: "[^a]i", iv: true, ic: true, chars: []rune{'a'}, out: []byte("b")},
+		{in: "B", val: "[^a]i", iv: true, ic: true, chars: []rune{'a'}, out: []byte("B")},
+
+		{in: "b", val: "[a-c]", ranges: []rune{'a', 'c'}, out: []byte("b")},
+		{in: "B", val: "[a-c]", ranges: []rune{'a', 'c'}, out: nil},
+		{in: "b", val: "[a-c]i", ic: true, ranges: []rune{'a', 'c'}, out: []byte("b")},
+		{in: "B", val: "[a-c]i", ic: true, ranges: []rune{'a', 'c'}, out: []byte("B")},
+		{in: "b", val: "[^a-c]", ranges: []rune{'a', 'c'}, iv: true, out: nil},
+		{in: "B", val: "[^a-c]", ranges: []rune{'a', 'c'}, iv: true, out: []byte("B")},
+		{in: "b", val: "[^a-c]i", iv: true, ic: true, ranges: []rune{'a', 'c'}, out: nil},
+		{in: "B", val: "[^a-c]i", iv: true, ic: true, ranges: []rune{'a', 'c'}, out: nil},
+		{in: "z", val: "[^a-c]i", iv: true, ic: true, chars: []rune{'a', 'c'}, out: []byte("z")},
+
+		{in: "b", val: "[c-a]", ranges: []rune{'c', 'a'}, out: nil},
+		{in: "B", val: "[c-a]i", ic: true, ranges: []rune{'c', 'a'}, out: nil},
+		{in: "B", val: "[^c-a]", iv: true, ranges: []rune{'c', 'a'}, out: []byte("B")},
+		{in: "B", val: "[^c-a]i", ic: true, iv: true, ranges: []rune{'c', 'a'}, out: []byte("B")},
+	}
+
+	for _, tc := range cases {
+		p := newParser("", []byte(tc.in))
+
+		// advance to the first rune
+		p.read()
+
+		var want interface{}
+		var match bool
+		if tc.out != nil {
+			want = tc.out
+			match = true
+		}
+		lbl := fmt.Sprintf("%q (%t-%t): %q", tc.val, tc.ic, tc.iv, tc.in)
+
+		classes := make([]*unicode.RangeTable, len(tc.classes))
+		for i, c := range tc.classes {
+			classes[i] = rangeTable(c)
+		}
+
+		got, ok := p.parseCharClassMatcher(&charClassMatcher{
+			val:        tc.val,
+			chars:      tc.chars,
+			ranges:     tc.ranges,
+			classes:    classes,
+			ignoreCase: tc.ic,
+			inverted:   tc.iv,
+		})
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("%s: want %v, got %v", lbl, tc.out, got)
+		}
+		if ok != match {
+			t.Errorf("%s: want match? %t, got %t", lbl, match, ok)
 		}
 		if p.pt.offset != len(tc.out) {
 			t.Errorf("%s: want offset %d, got %d", lbl, len(tc.out), p.pt.offset)
