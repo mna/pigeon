d96e9e68257bf7eec72de553f83ac9b54340cd17
Martin Angers <martin.n.angers@gmail.com>
2015-04-23 13:01:30 -0400

    vm: start implementation of matchers

--- vm/matchers.go
+++ vm/matchers.go
@@ -1,5 +1,42 @@
 package vm
 
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
+type ϡpeekReader interface {
+	peek() ϡsvpt
+	read()
+}
+
 type ϡmatcher interface {
-	match() bool
+	match(ϡpeekReader) bool
+}
+
+type ϡanyMatcher struct{}
+
+func (a ϡanyMatcher) match(pr ϡpeekReader) bool {
+	pt := pr.peek()
+	pr.read()
+	return pt.rn != utf8.RuneError
+}
+
+type ϡstringMatcher struct {
+	ignoreCase bool
+	value      string // value must be lowercase if ignoreCase is true
+}
+
+func (s ϡstringMatcher) match(pr ϡpeekReader) bool {
+	for _, want := range s.value {
+		pt := pr.peek()
+		if s.ignoreCase {
+			pt.rn = unicode.ToLower(pt.rn)
+		}
+		if pt.rn != want {
+			return false
+		}
+		pr.read()
+	}
+	return true
 }
