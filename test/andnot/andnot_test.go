e54ef0a95555da3fd3a0744793f0d021aa6e2bbb
Martin Angers <martin.n.angers@gmail.com>
2015-05-08 16:52:46 -0400

    make all tests pass with ffp errors

--- test/andnot/andnot_test.go
+++ test/andnot/andnot_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 // ABs must end in Bs, CDs must end in Ds
 var cases = map[string]string{
-	"":             "1:0 (0): no match found",
-	"a":            "1:1 (0): no match found",
+	"":             `1:0 (0): rule _: expected [ \t\n\r], got ""`,
+	"a":            `1:1 (1): rule AB: expected [ab], got ""`,
 	"b":            "",
 	"ab":           "",
-	"ba":           "1:1 (0): no match found",
+	"ba":           `1:2 (2): rule AB: expected [ab], got ""`,
 	"aab":          "",
-	"bba":          "1:1 (0): no match found",
-	"aabbaba":      "1:1 (0): no match found",
+	"bba":          `1:3 (3): rule AB: expected [ab], got ""`,
+	"aabbaba":      `1:7 (7): rule AB: expected [ab], got ""`,
 	"bbaabaaabbbb": "",
-	"abc":          "1:1 (0): no match found",
-	"c":            "1:1 (0): no match found",
+	"abc":          `1:3 (2): rule AB: expected [ab], got "c"`,
+	"c":            `1:1 (1): rule CD: expected [cd], got ""`,
 	"d":            "",
 	"cd":           "",
-	"dc":           "1:1 (0): no match found",
-	"dcddcc":       "1:1 (0): no match found",
+	"dc":           `1:2 (2): rule CD: expected [cd], got ""`,
+	"dcddcc":       `1:6 (6): rule CD: expected [cd], got ""`,
 	"dcddccdd":     "",
 }
 
