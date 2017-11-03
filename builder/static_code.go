0224bb3b3c14281497a7b0046b8f1c50697ab6c1
Martin Angers <martin.n.angers@gmail.com>
2017-11-03 11:19:41 -0400

    remove empty state optimization, fixes the emptyState test

--- builder/static_code.go
+++ builder/static_code.go
@@ -426,7 +426,7 @@ func newParser(filename string, b []byte, opts ...Option) *parser {
 		Stats:           &stats,
 		// start rule is rule [0] unless an alternate entrypoint is specified
 		entrypoint: g.rules[0].name,
-		emptyState: make(storeDict),
+		//emptyState: make(storeDict),
 	}
 	p.setOptions(opts)
 
@@ -519,7 +519,7 @@ type parser struct {
 	recoveryStack []map[string]interface{}
 
 	// emptyState contains an empty storeDict, which is used to optimize cloneState if global "state" store is not used.
-	emptyState storeDict
+	//emptyState storeDict
 }
 
 // push a variable set on the vstack.
@@ -700,9 +700,11 @@ func (p *parser) cloneState() storeDict {
 	}
 	// {{ end }} ==template==
 
-	if len(p.cur.state) == 0 {
-		return p.emptyState
-	}
+	/*
+		if len(p.cur.state) == 0 {
+			return p.emptyState
+		}
+	*/
 
 	state := make(storeDict, len(p.cur.state))
 	for k, v := range p.cur.state {
--- test/runeerror/runeerror.go
+++ test/runeerror/runeerror.go
@@ -493,7 +493,7 @@ func newParser(filename string, b []byte, opts ...Option) *parser {
 		Stats:           &stats,
 		// start rule is rule [0] unless an alternate entrypoint is specified
 		entrypoint: g.rules[0].name,
-		emptyState: make(storeDict),
+		//emptyState: make(storeDict),
 	}
 	p.setOptions(opts)
 
@@ -584,7 +584,7 @@ type parser struct {
 	recoveryStack []map[string]interface{}
 
 	// emptyState contains an empty storeDict, which is used to optimize cloneState if global "state" store is not used.
-	emptyState storeDict
+	//emptyState storeDict
 }
 
 // push a variable set on the vstack.
@@ -758,9 +758,11 @@ func (p *parser) cloneState() storeDict {
 		defer p.out(p.in("cloneState"))
 	}
 
-	if len(p.cur.state) == 0 {
-		return p.emptyState
-	}
+	/*
+		if len(p.cur.state) == 0 {
+			return p.emptyState
+		}
+	*/
 
 	state := make(storeDict, len(p.cur.state))
 	for k, v := range p.cur.state {
