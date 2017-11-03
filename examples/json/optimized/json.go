0bb44ad4ebd2ee8a1438071dc5450641f65ae3f7
Martin Angers <martin.n.angers@gmail.com>
2017-11-03 11:19:41 -0400

    remove empty state optimization, fixes the emptyState test

--- examples/json/optimized/json.go
+++ examples/json/optimized/json.go
@@ -1093,7 +1093,7 @@ func newParser(filename string, b []byte, opts ...Option) *parser {
 		Stats:           &stats,
 		// start rule is rule [0] unless an alternate entrypoint is specified
 		entrypoint: g.rules[0].name,
-		emptyState: make(storeDict),
+		//emptyState: make(storeDict),
 	}
 	p.setOptions(opts)
 
@@ -1178,7 +1178,7 @@ type parser struct {
 	recoveryStack []map[string]interface{}
 
 	// emptyState contains an empty storeDict, which is used to optimize cloneState if global "state" store is not used.
-	emptyState storeDict
+	//emptyState storeDict
 }
 
 // push a variable set on the vstack.
@@ -1326,9 +1326,11 @@ type Cloner interface {
 // clone and return parser current state.
 func (p *parser) cloneState() storeDict {
 
-	if len(p.cur.state) == 0 {
-		return p.emptyState
-	}
+	/*
+		if len(p.cur.state) == 0 {
+			return p.emptyState
+		}
+	*/
 
 	state := make(storeDict, len(p.cur.state))
 	for k, v := range p.cur.state {
