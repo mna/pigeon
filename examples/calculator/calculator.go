959c854b5574159e87e239197f897364380d3275
Martin Angers <martin.n.angers@gmail.com>
2017-11-03 10:23:57 -0400

    try to add a failing restore state test for ChoiceExpr, could not find a case

--- examples/calculator/calculator.go
+++ examples/calculator/calculator.go
@@ -1508,7 +1508,7 @@ func (p *parser) parseChoiceExpr(ch *choiceExpr) (interface{}, bool) {
 		// dummy assignment to prevent compile error if optimized
 		_ = altI
 
-		//state := p.cloneState()
+		state := p.cloneState()
 		p.pushV()
 		val, ok := p.parseExpr(alt)
 		p.popV()
@@ -1516,7 +1516,7 @@ func (p *parser) parseChoiceExpr(ch *choiceExpr) (interface{}, bool) {
 			p.incChoiceAltCnt(ch, altI)
 			return val, ok
 		}
-		//p.restoreState(state)
+		p.restoreState(state)
 	}
 	p.incChoiceAltCnt(ch, choiceNoMatch)
 	return nil, false
