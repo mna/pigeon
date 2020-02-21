938c240bfc2e833caabcd6531bb3b7570ffc713b
Philip Potter <philip.g.potter@gmail.com>
2020-02-21 20:07:46 +0000

    Cache `want` in litMatcher
    parseLitMatcher() was calling strconv.AppendQuote() each time, which my
    profiler discovered was costing a measurably large amount of runtime
    during parsing within dhall-golang.
    
    This commit adds a cache field to the litMatcher struct so that we only
    have to compute the `want` value once per litMatcher instead of once
    each time we parse with the litMatcher.

--- test/issue_70b/issue_70b.go
+++ test/issue_70b/issue_70b.go
@@ -401,6 +401,20 @@ type litMatcher struct {
 	pos        position
 	val        string
 	ignoreCase bool
+	wantCache  string
+}
+
+func (lit *litMatcher) want() string {
+	if lit.wantCache != "" {
+		return lit.wantCache
+	}
+	ignoreCase := ""
+	if lit.ignoreCase {
+		ignoreCase = "i"
+	}
+	val := string(strconv.AppendQuote([]byte{}, lit.val)) + ignoreCase // wrap 'lit.val' with double quotes
+	lit.wantCache = val
+	return val
 }
 
 // nolint: structcheck
@@ -1202,11 +1216,6 @@ func (p *parser) parseLitMatcher(lit *litMatcher) (interface{}, bool) {
 		defer p.out(p.in("parseLitMatcher"))
 	}
 
-	ignoreCase := ""
-	if lit.ignoreCase {
-		ignoreCase = "i"
-	}
-	val := string(strconv.AppendQuote([]byte{}, lit.val)) + ignoreCase // wrap 'lit.val' with double quotes
 	start := p.pt
 	for _, want := range lit.val {
 		cur := p.pt.rn
@@ -1214,13 +1223,13 @@ func (p *parser) parseLitMatcher(lit *litMatcher) (interface{}, bool) {
 			cur = unicode.ToLower(cur)
 		}
 		if cur != want {
-			p.failAt(false, start.position, val)
+			p.failAt(false, start.position, lit.want())
 			p.restore(start)
 			return nil, false
 		}
 		p.read()
 	}
-	p.failAt(true, start.position, val)
+	p.failAt(true, start.position, lit.want())
 	return p.sliceFrom(start), true
 }
 
